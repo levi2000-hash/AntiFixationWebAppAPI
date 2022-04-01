897eaebb78706843968d9d7caacd1569b0bb0cc0
Levi Noeninckx <levi.noeninckx@gmail.com>
2022-04-01 14:23:20 +0200

    update api.js

--- api.js
+++ api.js
@@ -1,34 +1,35 @@
 const express = require('express');
 const { json } = require('express/lib/response');
 const app = express()
-const http = require('http')
-const server = http.createServer(app);
+const cors = require('cors')
 
 app.use(express.json())
+app.use(cors())
 
 let patient_data = [
 	{
 		'name': 'levi',
 		'age': 21,
-		'fall_status': 0.5
+		'state': 0.5
 	},
 	{
 		'name': 'bob',
 		'age': 35,
-		'fall_status': 0,
+		'state': 0,
 	},
 	{
 		'name': 'elsa',
 		'age': 27,
-		'fall_status': 1,
+		'state': 1,
 	},
 ]
+
 
 app.get('/patients', (req, res) => {
 
 	//Get all data 	
 	console.log("here is all the data")
-	res.send(patient_data)
+	res.json(patient_data)
 })
 
 app.post('/patients', (req, res) => {
@@ -51,8 +52,8 @@ app.put('/patient/:id', (req, res) => {
 	//update patient data
 	let id = req.params.id - 1
 
-	patient_data[id].name = req.body.name 
-	patient_data[id].age = req.body.age 
+	patient_data[id].name = req.body.name
+	patient_data[id].age = req.body.age
 	patient_data[id].fall_status = req.body.fall_status
 
 	console.log("patient data updated")
@@ -64,21 +65,19 @@ app.delete('/patient/:id', (req, res) => {
 	console.log("patient deleted")
 	//TODO: implement delete functionality
 	let id = req.params.id - 1
-	try{
-		patient_data.splice(id,1);
-	} catch(err){
+	try {
+		patient_data.splice(id, 1);
+	} catch (err) {
 		console.log(err);
-	}	
+	}
 	res.sendStatus(200) //Currently returns error: patient ... can't be deleted
 })
 app.get('/patient/:id', (req, res) => {
 	//Get patient data
 	console.log("patient data")
-	res.send(patient_data[req.params.id-1])
+	res.send(patient_data[req.params.id - 1])
 })
-
-
-app.listen(3000, () => {
-	console.log('listening on port 3000')
+app.listen(3001, () => {
+	console.log('listening on port 3001')
 }
 )
